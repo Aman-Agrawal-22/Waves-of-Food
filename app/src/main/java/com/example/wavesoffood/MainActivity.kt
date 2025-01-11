befe49c514747dfaebfc86e93893014af25ab7dd
Aman <amanag22.aa@gmail.com>
2025-01-11 18:08:35 +0530

    created profile page and notifications bar

--- app/src/main/java/com/example/wavesoffood/MainActivity.kt
+++ app/src/main/java/com/example/wavesoffood/MainActivity.kt
@@ -8,15 +8,22 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.wavesoffood.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         var NavController = findNavController(R.id.fragmentContainerView2)
         var bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomnav.setupWithNavController(NavController)
+        binding.notificationButton.setOnClickListener {
+            val bottomSheetFragment = Notification_Bottom_Fragment()
+            bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
+        }
     }
 }
