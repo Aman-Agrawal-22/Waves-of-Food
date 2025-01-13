f6bd1acbe28daa813546c6cb790ff51e80516dda
Aman <amanag22.aa@gmail.com>
2025-01-13 12:24:02 +0530

    created PayOut Activity and Congratulations Bottom Sheet Fragment

--- app/src/main/java/com/example/wavesoffood/Fragment/CartFragment.kt
+++ app/src/main/java/com/example/wavesoffood/Fragment/CartFragment.kt
@@ -1,11 +1,14 @@
 package com.example.wavesoffood.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.wavesoffood.CongratsBottomSheet
+import com.example.wavesoffood.PayOutActivity
 import com.example.wavesoffood.R
 import com.example.wavesoffood.adapter.CartAdapter
 import com.example.wavesoffood.databinding.FragmentCartBinding
@@ -33,6 +36,14 @@ class CartFragment : Fragment() {
         val adapter = CartAdapter(ArrayList(cartFoodName), ArrayList(cartFoodPrice), ArrayList(cartImage))
         binding.cartRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.cartRecyclerView.adapter = adapter
+
+        binding.proceedButton.setOnClickListener {
+            val intent = Intent(requireContext(), PayOutActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
         return binding.root
     }
 
